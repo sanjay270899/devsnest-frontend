6f6169da1afd3efa6c93ac2ca8f3a916640281b4
shiv-user <shivranjan1497@gmail.com>
2020-07-16 15:54:09 +0530

    changes:removed console ,type defined props

--- src/romponents/Curriculum.tsx
+++ src/romponents/Curriculum.tsx
@@ -1,53 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import axios from '../config/axios.config';
-import Header from './Header'
-import Chapter from './Chapter'
-import {Redirect} from "react-router";
-
+import React, { useEffect, useState } from "react";
+import axios from "../config/axios.config";
+import Header from "./Header";
+import Chapter from "./Chapter";
+import { Redirect } from "react-router";
 
 function Curriculum(props: any) {
   const [xhapters, setChapters] = useState([[]]);
 
   const id: number = props.match.params.id;
-  let completed:Number
-  let token: string = localStorage.getItem("Token") || '';
+  let percentageCompleted: number;
+  let token: string = localStorage.getItem("Token") || "";
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const fetchDetails = async () => {
-    if( token != ''){
+    if (token != "") {
       const res = await axios.get(`/api/curriculums/${id}/chapter`, {
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      })
+          Authorization: `Bearer ${token}`,
+        },
+      });
       const { data } = res;
       let allSubTasks = data.data.map((item: any) => {
         const task = {
           id: item.id,
           text: item.name,
           slug: item.slug,
-        }
+        };
         return task;
       });
       setChapters(allSubTasks);
     }
-
   };
   useEffect(() => {
-    fetchDetails()
+    fetchDetails();
   }, []);
 
-  const fetchProgress=async ()=>{
-    const response = await(axios.get(`//please add the api here`, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-        }
-    }))
-    completed={response.progress}//please get the completed data
-  }
-  useEffect(()=>{fetchProgress()},[])
-  if(token == ''){
-    return <Redirect to={"/login"}/>;
-  }
   return (
     <>
       <Header />
@@ -56,16 +42,14 @@ function Curriculum(props: any) {
           <>
             <Chapter
               task={task}
-              completed = {completed}
-              updateAllTasks={ () => fetchDetails()}
+              percentageCompleted={percentageCompleted}
+              updateAllTasks={() => fetchDetails()}
             />
           </>
-        )
+        );
       })}
-
     </>
   );
-
 }
 
 export default Curriculum;
