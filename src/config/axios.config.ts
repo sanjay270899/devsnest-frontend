fbd2c443e4ea0ee7d9b28b57f7fd837a74a595de
anujydv <anujy647@gmail.com>
2020-08-05 03:58:40 +0530

    production build

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: 'http://13.232.2.76:3000/',
+  baseURL: 'http://3.7.23.30:3000/',
 });
 
 export default instance;
