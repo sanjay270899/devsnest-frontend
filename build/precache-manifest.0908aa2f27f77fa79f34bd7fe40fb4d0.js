b300188b3ee3e039c3c0d715416a92685ba6fb05
anujydv <anujy647@gmail.com>
2020-08-10 00:48:23 +0530

    prod build

---
+++ build/precache-manifest.0908aa2f27f77fa79f34bd7fe40fb4d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'db2abe43ad8a79a00373214cc4d36bdd',
+    url: '/index.html',
+  },
+  {
+    revision: 'bde25094ed5c8e066f25',
+    url: '/static/css/2.32a52a2a.chunk.css',
+  },
+  {
+    revision: 'dd6964678c332486d137',
+    url: '/static/css/main.6d8c45ae.chunk.css',
+  },
+  {
+    revision: 'bde25094ed5c8e066f25',
+    url: '/static/js/2.6b54e040.chunk.js',
+  },
+  {
+    revision: '6c62bde31aede2583549d1e23e623ad7',
+    url: '/static/js/2.6b54e040.chunk.js.LICENSE.txt',
+  },
+  {
+    revision: 'dd6964678c332486d137',
+    url: '/static/js/main.f4bfa87e.chunk.js',
+  },
+  {
+    revision: 'ddbb78fcddb0053a49e2',
+    url: '/static/js/runtime-main.6368b004.js',
+  },
+]);
