b7f02b6014652cd59b55ebfdb281bd64953b8f0a
komalahire <komala18@navgurukul.org>
2020-08-10 21:28:42 +0530

    Have added side menu bar in navigation.

--- src/romponents/Header.tsx
+++ src/romponents/Header.tsx
@@ -1,38 +1,107 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { Link } from 'react-router-dom';
+import MenuItem from '@material-ui/core/MenuItem';
+import FeedbackIcon from '@material-ui/icons/Feedback';
 import ReactGA from 'react-ga';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+const drawerWidth = 240;
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  })
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
 
-export default function Header() {
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+export default function PersistentDrawerLeft() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
   const [auth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+  // const open = Boolean(anchorEl);
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
   };
 
   const handleClose = () => {
@@ -48,12 +117,16 @@ export default function Header() {
   };
 
   return (
-    <div>
-      <AppBar position="static">
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
         <Toolbar>
           <IconButton
-            edge="start"
-            className={classes.menuButton}
             color="inherit"
             aria-label="menu"
             aria-controls="menu-appbar"
@@ -62,7 +135,7 @@ export default function Header() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" noWrap className={classes.title}>
             <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>
               Devnest
             </Link>
@@ -75,56 +148,75 @@ export default function Header() {
                 aria-haspopup="true"
                 color="inherit"
               >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose} style={{ color: '#000' }}>
-                  <Link
-                    to="/profile"
-                    style={{ color: '#000', textDecoration: 'none' }}
-                  >
-                    Profile
-                  </Link>
-                </MenuItem>
+                Profile
+              </Link>
+            </MenuItem>
+          </ListItem>
 
-                <MenuItem onClick={handleClose} style={{ color: '#000' }}>
-                  <Link
-                    to="/settings"
-                    style={{ color: '#000', textDecoration: 'none' }}
-                  >
-                    Settings
+        <ListItem button>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+
+          <MenuItem onClick={handleClose} style={{ color: '#000' }}>
+            <Link
+              to="/Setting"
+              style={{
+                color: '#000',
+                textDecoration: 'none',
+              }}
+            >
+              Settings
+              </Link>
+          </MenuItem>
+        </ListItem>
+
+        <ListItem button>
+          <ListItemIcon>
+            <FeedbackIcon />
+          </ListItemIcon>
+
+          <MenuItem onClick={handleClose} style={{ color: '#000' }}>
+            <Link
+              to="/"
+              style={{
+                color: '#000',
+                textDecoration: 'none',
+              }}
+            >
+              <MenuItem onClick={handleClose} style={{ color: '#000' }}>
+                <Link
+                  to="/profile"
+                  style={{ color: '#000', textDecoration: 'none' }}
+                >
+                  Profile
                   </Link>
-                </MenuItem>
-                <MenuItem onClick={handleClose} style={{ color: '#000' }}>
-                  <Link
-                    to="/gbu"
-                    style={{ color: '#000', textDecoration: 'none' }}
-                  >
-                    Gbu
+              </MenuItem>
+
+              <MenuItem onClick={handleClose} style={{ color: '#000' }}>
+                <Link
+                  to="/settings"
+                  style={{ color: '#000', textDecoration: 'none' }}
+                >
+                  Settings
                   </Link>
-                </MenuItem>
-                <MenuItem onClick={handleClick}>
-                  <Link
-                    to="/login"
-                    style={{ color: '#000', textDecoration: 'none' }}
-                  >
-                    Logout
+              </MenuItem>
+              <MenuItem onClick={handleClose} style={{ color: '#000' }}>
+                <Link
+                  to="/gbu"
+                  style={{ color: '#000', textDecoration: 'none' }}
+                >
+                  Gbu
                   </Link>
-                </MenuItem>
+              </MenuItem>
+              <MenuItem onClick={handleClick}>
+                <Link
+                  to="/login"
+                  style={{ color: '#000', textDecoration: 'none' }}
+                >
+                  Logout
+                  </Link>
+              </MenuItem>
               </Menu>
             </div>
           )}
