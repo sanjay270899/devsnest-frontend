4fdfa314236dd37f97c44f6a1afe8209627ecc4b
komalahire <komala18@navgurukul.org>
2020-09-08 16:31:47 +0530

    Redesigned the Curriculum page.

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,10 +1,18 @@
+// import axios, { AxiosInstance } from 'axios';
+
+// const instance: AxiosInstance = axios.create({
+//   baseURL:
+//     process.env.NODE_ENV === 'production'
+//       ? process.env.REACT_APP_BASE_PROD_IP
+//       : process.env.REACT_APP_BASE_DEV_IP,
+// });
+
+// export default instance;
+
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_BASE_PROD_IP
-      : process.env.REACT_APP_BASE_DEV_IP,
+  baseURL: 'http://3.7.23.30:3000',
 });
 
 export default instance;
