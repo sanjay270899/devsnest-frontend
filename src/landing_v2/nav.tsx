30a883da19cc8186c64006c04369c7d61dd4e58d
pawnikhanna <pawnikhanna12@gmail.com>
2021-02-06 23:18:52 +0530

    sticky nav

--- src/landing_v2/nav.tsx
+++ src/landing_v2/nav.tsx
@@ -21,48 +21,48 @@ function Nav(props) {
     'Building a platform of peer learning enabling students to learn → grow → develop projects together';
   return (
     <section className="hero rowx dark">
-      <div className="main-nav">
-        <div className="main-nav__nav_container">
-          <div className="main-nav__nav_left">
-            <NavHashLink to="/">
-              <div className="logo-container"></div>
-            </NavHashLink>
-          </div>
-          <div className="main-nav__nav_right">
-            <a
-              href="https://docs.google.com/forms/d/1FDUajSVjIgG-7hBBl5ekOpHxurVi-DvqQ1AcbwiJpuI/edit?ts=5f4a681a"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="nav-item"
-            >
-              Apply
-            </a>
-            <NavHashLink to="/#mentors" className="nav-item">
-              Mentors
-            </NavHashLink>
-            <NavHashLink to="/faqs#read" className="nav-item">
-              FAQs
-            </NavHashLink>
-            <NavHashLink to="/#curriculum" className="nav-item">
-              Curriculum
-            </NavHashLink>
-          </div>
+      <div className="nav-bar">
+        <div className="nav-bar__left-side">
+          <NavHashLink to="/">
+            <div className="nav-bar__logo-container"></div>
+          </NavHashLink>
+        </div>
+        <div
+          className={classNames('nav-bar__right-side', {
+            'show-with-style': showNav,
+          })}
+        >
+          <a
+            href="https://docs.google.com/forms/d/1FDUajSVjIgG-7hBBl5ekOpHxurVi-DvqQ1AcbwiJpuI/edit?ts=5f4a681a"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-item"
+          >
+            Apply
+          </a>
+          <NavHashLink to="/#mentors" className="nav-item">
+            Mentors
+          </NavHashLink>
+          <NavHashLink to="/faqs#read" className="nav-item">
+            FAQs
+          </NavHashLink>
+          <NavHashLink to="/#curriculum" className="nav-item">
+            Curriculum
+          </NavHashLink>
+        </div>
+        <div className="nav-bar__bar-container">
+          <img
+            alt="icon"
+            onClick={() => setShowNav(!showNav)}
+            className="nav-bar__hamburger"
+            src="./bars.svg"
+          />
         </div>
       </div>
       <div className="hero__left-side colx">
         <img src="./hero2.jpg" alt="hero" />
       </div>
       <div className="hero__right-side colx">
-        <div className="nav-bar">
-          <div className="nav-bar__bar-container">
-            <img
-              alt="icon"
-              onClick={() => setShowNav(!showNav)}
-              className="nav-bar__hamburger"
-              src="./bars.svg"
-            />
-          </div>
-        </div>
         <div className="content">
           {headLine}
           <div className="descpx hero__descp">{subHeading}</div>
