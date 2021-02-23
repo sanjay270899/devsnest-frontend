c6f0dd6663394c0034ee9fb5f8c59493a911ce70
patheticGeek <geekpathetic@gmail.com>
2021-02-23 21:12:14 +0530

    Example page added

---
+++ src/pages/Example.js
@@ -0,0 +1,38 @@
+// Imports
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useActions } from '../hooks/useActions';
+import { login } from '../actions/loginActions';
+
+// Other smaller components
+function Input({ value, setValue }) {
+  return <input value={value} onChange={(e) => setValue(e.target.value)} />;
+}
+
+// Main Component
+function Example() {
+  // State
+  const [value, setValue] = useState('');
+
+  // Redux state and actions
+  const loginState = useSelector((state) => state.loginState);
+  const actions = useActions({ login });
+
+  // Effects
+  useEffect(() => {
+    console.log('mounted');
+  }, []);
+
+  // return
+  return (
+    <div style={{ paddingTop: '92px' }}>
+      <div className="mb-2">loginState: {JSON.stringify(loginState)}</div>
+      <button onClick={() => actions.login({})}>Login</button>
+
+      <Input value={value} setValue={setValue} />
+    </div>
+  );
+}
+
+// Export main component
+export default Example;
