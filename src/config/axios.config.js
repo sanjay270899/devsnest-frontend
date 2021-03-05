80a4768413af775ac84dbb3fb90a5bf4ee14221a
patheticGeek <geekpathetic@gmail.com>
2021-03-05 15:00:17 +0530

    added logging middleware for axios

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 import { API_BASE_URL } from '../constants/api';
 import { loadStorage } from '../utils/localStorage';
+import myLog from '../utils/myLog';
 
 const axios = Axios.create({
   baseURL: API_BASE_URL,
@@ -24,5 +25,31 @@ axios.interceptors.request.use(
     return Promise.reject(err);
   }
 );
+
+axios.interceptors.request.use((request) => {
+  myLog(
+    `------------------ \nRequest to ${request.method.toUpperCase()} ${
+      request.url
+    } with params`,
+    request.params,
+    `and data`,
+    request.data
+  );
+  return request;
+});
+
+axios.interceptors.response.use((response) => {
+  myLog(
+    `------------------ \nResponse from ${response.config.method.toUpperCase()} ${
+      response.config.url
+    } with params`,
+    response.config.params,
+    `and data`,
+    response.config.data,
+    `and got response data ->`,
+    response.data
+  );
+  return response;
+});
 
 export default axios;
