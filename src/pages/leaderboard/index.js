4bea41caa477cc647db0280fa4390d654ea4cfa7
patheticGeek <geekpathetic@gmail.com>
2021-03-07 12:25:52 +0530

    added leaderboard autoloading list

--- src/pages/leaderboard/index.js
+++ src/pages/leaderboard/index.js
@@ -1,23 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
+import useScroll from '../../hooks/useScroll';
 import axios from '../../config/axios.config';
 import { API_ENDPOINTS } from '../../constants/api';
 import './leaderboard.scss';
 
 export default function Leaderboard() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [leaderboardData, setLeaderboardData] = useState({});
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const user = useSelector((state) => state.loginState.user);
+  const scrollContainerRef = useRef();
+  const [, , scrollTop] = useScroll(scrollContainerRef);
 
   useEffect(() => {
+    const element = scrollContainerRef.current;
+    if (!element) return;
+    if (
+      element.scrollHeight - element.scrollTop <= element.clientHeight + 100 &&
+      currentPage < leaderboardData.count
+    ) {
+      setIsLoadingMore(true);
+      setCurrentPage(currentPage + 1);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scrollTop]);
+
+  useEffect(() => {
+    setIsLoadingMore(true);
     axios
       .get(`${API_ENDPOINTS.LEADERBOARD}?page=${currentPage}&size=20`)
       .then((res) => {
-        setLeaderboardData(res.data);
+        // if data not loaded set data
+        // otherwise append to current data
+        if (!leaderboardData.scoreboard) {
+          setLeaderboardData(res.data);
+        } else {
+          setLeaderboardData({
+            ...leaderboardData,
+            scoreboard: [...leaderboardData.scoreboard, ...res.data.scoreboard],
+          });
+        }
+        setIsLoadingMore(false);
         setIsLoading(false);
       })
       .catch((e) => console.error(e));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPage]);
 
   if (isLoading) {
@@ -31,27 +60,38 @@ export default function Leaderboard() {
   return (
     <div className="leaderboard">
       <div className="leaderboard__container row flex-row-reverse">
-        <div className="col-md-12">
-          <div className="leaderboard__table">
+        <div className="col-md-12" style={{ height: '100%' }}>
+          <div className="leaderboard__table pb-3">
             <div className="leaderboard__table-header row py-3 my-1 mx-4">
               <div className="h5 mb-0 text-center col-2">Rank</div>
               <div className="h5 mb-0 col-6">Name</div>
               <div className="h5 mb-0 text-center col-4">Score</div>
             </div>
 
-            {leaderboardData.scoreboard &&
-              leaderboardData.scoreboard.map((item, index) => (
-                <div
-                  className={`row py-3 mx-4 ${
-                    item.id === user.id ? 'current-user my-2' : 'my-0'
-                  }`}
-                  key={item.id}
-                >
-                  <div className="text-center col-2">{index + 1}.</div>
-                  <div className="col-6">{item.name}</div>
-                  <div className="text-center col-4">{item.score || '0'}</div>
+            <div className="leaderboard__table-rows" ref={scrollContainerRef}>
+              {leaderboardData.scoreboard &&
+                leaderboardData.scoreboard.map((item, index) => (
+                  <div
+                    className={`row py-3 mx-4 ${
+                      item.id === user.id ? 'current-user my-2' : 'my-0'
+                    }`}
+                    key={item.id}
+                  >
+                    <div className="text-center col-2">{index + 1}.</div>
+                    <div className="col-6">{item.name}</div>
+                    <div className="text-center col-4">{item.score || '0'}</div>
+                  </div>
+                ))}
+
+              {isLoadingMore && (
+                <div className="d-flex my-3">
+                  <div
+                    class="spinner-border text-primary mx-auto"
+                    role="status"
+                  />
                 </div>
-              ))}
+              )}
+            </div>
           </div>
         </div>
       </div>
