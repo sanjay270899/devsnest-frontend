733c307bb1a36a6c85fd268cc9494460355553a0
ShrishtiAgarwal <shrishtiagarwal21@gmail.com>
2021-05-30 17:13:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import Dashboard from './pages/Dashboard';
 import Challenges from './pages/Challanges';
 import Leaderboard from './pages/Leaderboard';
 import Groups from './pages/Groups';
+import UserDashboard from './pages/UserDashboard';
 
 toast.configure({
   bodyStyle: {
@@ -53,6 +54,7 @@ function App() {
             loggedInComponent={Dashboard}
             loggedOutComponent={Landing}
           />
+          <Route exact path="/me" component={UserDashboard} />
           <Route exact path="/faqs" component={Faq} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/login/callback" component={LoginCallback} />
