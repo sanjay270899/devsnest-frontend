a816b0e7cb6915ae9b303544ee03ceb7a741181c
Sanjay Prajapati <sanjay270899@gmail.com>
2021-06-08 01:04:34 +0530

    🐛 NaN Bug in ProblemDetails Component

--- src/pages/dashboard/ProblemsDetails.js
+++ src/pages/dashboard/ProblemsDetails.js
@@ -2,17 +2,25 @@ import React from 'react';
 
 export const ProblemsDetails = ({ user }) => {
   // Solved Question by User by Difficulty
-  const easy = user.solved?.easy;
-  const medium = user.solved?.medium;
-  const hard = user.solved?.hard;
+  let easy = user.solved?.easy;
+  easy = easy ? easy : 0;
+  let medium = user.solved?.medium;
+  medium = medium ? easy : 0;
+  let hard = user.solved?.hard;
+  hard = hard ? easy : 0;
 
   // Total Queustion by Difficulty
-  const easy_t = user.total_by_difficulty?.easy;
-  const medium_t = user.total_by_difficulty?.medium;
-  const hard_t = user.total_by_difficulty?.hard;
+  let easy_t = user.total_by_difficulty?.easy;
+  easy_t = easy_t ? easy : 0;
+  let medium_t = user.total_by_difficulty?.medium;
+  medium_t = medium_t ? easy : 0;
+  let hard_t = user.total_by_difficulty?.hard;
+  hard_t = hard_t ? easy : 0;
 
-  const solved = easy + medium + hard; // Total Solved
-  const total = easy_t + medium_t + hard_t; // Total Questions
+  let solved = easy + medium + hard; // Total Solved
+  solved = solved ? easy : 0;
+  let total = easy_t + medium_t + hard_t; // Total Questions
+  total = total ? easy : 0;
 
   return (
     <div
