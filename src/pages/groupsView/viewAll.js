fcf3cac59b1657017ece88e0318a394490575de0
Naman <namankumarnarula@gmail.com>
2021-06-08 23:24:20 +0530

    fixed a bug which showed other groups demarcation to normal user

--- src/pages/groupsView/viewAll.js
+++ src/pages/groupsView/viewAll.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import TeamCard from './teamCard';
-import { Link } from 'react-router-dom';
+
 import styles from '../../assets/css/groupsView.module.scss';
 import { API_ENDPOINTS } from '../../constants/api';
 import axios from '../../config/axios.config';
-import { useSelector } from 'react-redux';
 
 export default function AllGroups() {
   const [allTeams, setAllTeams] = useState([]);
   const [myTeam, setMyTeam] = useState({});
-
-  const { group_id } = useSelector((state) => state.loginState.user);
 
   useEffect(() => {
     async function getAllTeams() {
@@ -40,9 +37,13 @@ export default function AllGroups() {
       <div id={styles.myTeam}>
         <TeamCard key={myTeam.id} {...myTeam} />
       </div>
-      <p
-        dangerouslySetInnerHTML={{ __html: '&nbsp&nbspOther Groups&nbsp&nbsp' }}
-      ></p>
+      {allTeams.length > 1 && (
+        <p
+          dangerouslySetInnerHTML={{
+            __html: '&nbsp&nbspOther Groups&nbsp&nbsp',
+          }}
+        ></p>
+      )}
       <div className={styles.TeamView}>
         {allTeams.map((team) => (
           <TeamCard key={team.id} {...team}></TeamCard>
