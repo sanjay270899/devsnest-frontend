8ba99080768116e045548f2e720bc86d4d40e90e
patheticGeek <geekpathetic@gmail.com>
2021-06-13 01:53:28 +0530

    🐛 Fix groups page

--- src/pages/AllTeams.js
+++ src/pages/AllTeams.js
@@ -5,13 +5,19 @@ import styles from '../assets/css/groupsView.module.scss';
 import { API_ENDPOINTS } from '../constants/api';
 import axios from '../config/axios.config';
 import myLog from '../utils/myLog';
+import no_data from '../assets/images/groups/no_data.svg';
 
 export default function AllGroups() {
   const [allTeams, setAllTeams] = useState([]);
   const [myTeam, setMyTeam] = useState({});
   const my_group_id = useSelector((state) => state.loginState.user.group_id);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
+    if (!my_group_id) return;
+
     async function getAllTeams() {
+      setIsLoading(true);
       try {
         const {
           data: { data },
@@ -32,9 +38,43 @@ export default function AllGroups() {
       } catch (e) {
         myLog(e);
       }
+      setIsLoading(false);
     }
+
     getAllTeams();
-  }, []);
+  }, [my_group_id]);
+
+  if (!my_group_id) {
+    return (
+      <div className="groups d-flex flex-column align-items-center justify-content-center px-3">
+        <img
+          className="img-fluid"
+          src={no_data}
+          alt="New things are coming soon!"
+        />
+        <h5 className="text-center text-muted mt-5 mb-2">
+          You're not in any group yet.
+          <br />
+          Join our server and find one that fits you!
+        </h5>
+        <a
+          href="https://discord.gg/DVmruvFfDN"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join our discord server
+        </a>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="groups d-flex">
+        <div className="spinner-border text-primary m-auto" role="status" />
+      </div>
+    );
+  }
 
   return (
     <>
