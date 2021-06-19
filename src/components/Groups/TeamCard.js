8af61fe9154e43a9a519bd9faf8904d93a37a319
patheticGeek <geekpathetic@gmail.com>
2021-06-19 19:53:08 +0530

    💄 Fix group page design

--- src/components/Groups/TeamCard.js
+++ src/components/Groups/TeamCard.js
@@ -7,7 +7,7 @@ import icons from '../../utils/getIcons';
 
 export default function TeamCard({ name, owner_name, co_owner_name, slug }) {
   return (
-    <div className="card w-33 group-card">
+    <div className="card w-33 my-3 group-card">
       <div className="card-header d-flex align-items-center py-2 px-3 group-card-header">
         <h4 className="text-truncate mb-0 mr-3">{name}</h4>
         <Link
@@ -19,8 +19,10 @@ export default function TeamCard({ name, owner_name, co_owner_name, slug }) {
         </Link>
       </div>
       <div className="card-body p-3">
-        <p className="mb-2">Team Leader: {owner_name || 'No owner'}</p>
-        <p className="mb-0">
+        <p className="mb-2 text-truncate">
+          Team Leader: {owner_name || 'No owner'}
+        </p>
+        <p className="mb-0 text-truncate">
           Vice Team Leader: {co_owner_name || 'No co-owner'}
         </p>
       </div>
