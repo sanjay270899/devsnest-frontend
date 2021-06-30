40a1971adf4b76880e845b33c7ef05c2a00378df
Sanjay Prajapati <sanjay270899@gmail.com>
2021-06-30 17:36:59 +0530

    🐛 Store initial state update & username toast fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,11 @@ import loginReducer from './slices/loginSlice';
 
 const preloadedState = loadStorage(`${window.origin}/state`) || {};
 
+// Prevent the access of Private Components by Conditional Route before useAuth "/me" API
+if (preloadedState.login) {
+  preloadedState.login.isLoading = true;
+}
+
 const store = configureStore({
   preloadedState,
   reducer: {
